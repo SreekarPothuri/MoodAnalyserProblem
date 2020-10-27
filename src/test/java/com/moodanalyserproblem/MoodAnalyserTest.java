75ad4c002bfd242ddbf0f3600a693bf142aa493e
Your Name <you@example.com>
2020-10-27 21:05:42 +0530

    Added: Custom exception test cases

--- src/test/java/com/moodanalyserproblem/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyserproblem/MoodAnalyserTest.java
@@ -2,7 +2,11 @@ package com.moodanalyserproblem;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.customexp.MoodAnalysisException;
 
 public class MoodAnalyserTest {
 
@@ -11,18 +15,49 @@ public class MoodAnalyserTest {
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionHappy() {
 		mood = new MoodAnalyser("I am in Happy Mood");
-		assertEquals("HAPPY", mood.analyseability());
+		try {
+			assertEquals("HAPPY", mood.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
 	}
 
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionSad() {
 		mood = new MoodAnalyser("I am in Sad Mood");
-		assertEquals("SAD", mood.analyseability());
+		try {
+			assertEquals("SAD", mood.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
+	}	
+		
+	@Test
+	public void testgivenNullMoodShouldReturnHaappy_thenAssertionHappy() {
+		mood = new MoodAnalyser(null);
+		try {
+			assertEquals("HAPPY", mood.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
 	}
 	
 	@Test
-	public void testgivenNullMoodShouldReturnHaappy_thenAssertionHappy() {
-		mood = new MoodAnalyser();
-		assertEquals("HAPPY", mood.analyseability());
+	public void testgivenNullMoodShouldThrowMoodAnalysis_thenAssertionHappy() {
+		mood = new MoodAnalyser(null);
+		try {
+			MoodAnalyser.analyseability();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals("Please enter proper message",e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testgivenEmptyMoodShouldThrowMoodAnalysis_thenAssertionHappy() {
+		mood = new MoodAnalyser(" ");
+		try {
+			MoodAnalyser.analyseability();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals("Please enter other than empty message",e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
