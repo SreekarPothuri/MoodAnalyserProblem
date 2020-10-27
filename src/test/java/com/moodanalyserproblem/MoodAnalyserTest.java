df3ed7cba1fa78c5f950d999036406508e611c18
Your Name <you@example.com>
2020-10-27 19:09:40 +0530

    Refactored: Included constructors

--- src/test/java/com/moodanalyserproblem/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyserproblem/MoodAnalyserTest.java
@@ -2,31 +2,21 @@ package com.moodanalyserproblem;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
 	static MoodAnalyser mood;
 
-	@BeforeClass
-	public static void createMoodAnalyserObj() {
-		mood = new MoodAnalyser();
-	}
-	
-	@AfterClass
-	public static void nullObj() {
-		mood = null;
-	}
-
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionHappy() {
-		assertEquals("HAPPY", mood.analyseability("I am in Happy Mood"));
+		mood = new MoodAnalyser("I am in Happy Mood");
+		assertEquals("HAPPY", mood.analyseability());
 	}
 
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionSad() {
-		assertEquals("SAD", mood.analyseability("I am in Sad Mood"));
+		mood = new MoodAnalyser("I am in Sad Mood");
+		assertEquals("SAD", mood.analyseability());
 	}
 }
